1f09785d240ebb923dc423bf13aec2c0c4eeec71
Chris Brown <chris@greenhilldigital.com>
2015-11-09 22:16:03 +0000

    Add basic translation function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 new Vue({
-	
+	el: '#app',
+	data: {
+		input: '',
+	    output: '',
+	    newOutput: '',
+	    engToAl: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+	    alToEng: 'ypltavkrezgmshubxncdijfqowYPLTAVKREZGMSHUBXNCDIJFXOW'
+	},
+	methods: {
+		translate: function() {
+			this.newOutput = '';
+			for (var i = 0, len = this.input.length; i < len; i++) {
+				this.newOutput += this.alToEng[this.engToAl.indexOf(this.input[i])];
+			}
+			this.output = this.newOutput;
+		}
+	}
 });
