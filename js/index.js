a6affa42441d3523819d73c9608fb7f4f47a513d
Chris Brown <chris@greenhilldigital.com>
2015-11-15 16:54:31 +0000

    Refactoring to use Vue's .$watch function rather than calling function on keyup

--- js/index.js
+++ js/index.js
@@ -1,19 +1,28 @@
-new Vue({
-	el: '#app',
-	data: {
-		input: '',
-	    output: '',
-	    newOutput: '',
-	    engToAl: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
-	    alToEng: 'ypltavkrezgmshubxncdijfqowYPLTAVKREZGMSHUBXNCDIJFXOW'
-	},
-	methods: {
-		translate: function() {
-			this.newOutput = '';
-			for (var i = 0, len = this.input.length; i < len; i++) {
-				this.newOutput += this.alToEng[this.engToAl.indexOf(this.input[i])];
-			}
-			this.output = this.newOutput;
-		}
+(function main() {
+	var engToAl = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	var alToEng = 'ypltavkrezgmshubxncdijfqowYPLTAVKREZGMSHUBXNCDIJFXOW';
+
+	function isLetter(str) {
+	  return str.length === 1 && str.match(/[a-z]/i);
 	}
-});
+
+	var vm = new Vue({
+		el: '#app',
+		data: {
+			input: '',
+		    output: ''
+		}
+	});
+
+	vm.$watch('input', function(newVal) {
+		var newOutput = '';
+		for (var i = 0, len = newVal.length; i < len; i++) {
+			if (isLetter(newVal[i])) {
+				newOutput += alToEng[engToAl.indexOf(newVal[i])];
+			} else {
+				newOutput += newVal[i];
+			}
+		}
+		this.output = newOutput;
+	});
+})();
